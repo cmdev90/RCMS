8d3560c1320a27d887bc125d2b5a47f515cccfd8
Cherlton Millette <cherlton.millette@gmail.com>
2014-12-02 16:59:00 -0400

    Intentions to create a core module.

--- server/worker.js
+++ server/worker.js
@@ -6,13 +6,13 @@ var datastore = require('./datastore.js')(auth)
 
 
 // we create a new http server that does nothing but open a connection for socketio to make use of.
-var app = require('http').createServer();
-var io = require('socket.io')(app);
+var app = require('http').createServer()
+var io = require('socket.io')(app)
 
 // Start listening on the port set by the environment.
-app.listen(8181);
+app.listen(8181)
 
-var connectedSockets = {}; // each connected socket will be stored in this object so it can be refrenced easy
+var connectedSockets = {} // each connected socket will be stored in this object so it can be refrenced easy
 
 // Create an event for wne clients connect.
 io.on('connection', function (socket){
@@ -20,34 +20,34 @@ io.on('connection', function (socket){
   // event 'authenticate' to listen for the response before making the request.
   socket.on('authenticate', function (data){
     if (data.type && data.type === 'authenticate')
-      authenticateClient(data.identity, data.secret, this);
+      authenticateClient(data.identity, data.secret, this)
     else if (data.type && data.type === 'create')
-      createClient(data, this);
+      createClient(data, this)
     else
-      socket.emit('wtf'); // What a Terrible Failure!
-  });
+      socket.emit('wtf') // What a Terrible Failure!
+  })
 
   // Now ask the client to authenticate themselves with the service
   // by emitting an 'authenticate' event over the socket.
-  socket.emit('authenticate');
-});
+  socket.emit('authenticate')
+})
 
 
 var debug = {
   log: function (message) {
-    console.log(message);
+    console.log(message)
   }
-};
+}
 
 function connectSocket(identity, socket) {
-  debug.log('Adding socket (' + socket.id + ') with identity (' + identity + ')');
-  socket['__identity__'] = identity; // The socket must know its own identity!
-  connectedSockets[identity] = socket; // Create an association between the User identity and this socket.
-  setEventListeners(socket); // We need to setup the server to listen to the various socket events now that the client is authenticated.
-  socket.emit('authenticated'); // Let the client know they have been authenticated by emitting an 'authenticated' event.
+  debug.log('Adding socket (' + socket.id + ') with identity (' + identity + ')')
+  socket['__identity__'] = identity // The socket must know its own identity!
+  connectedSockets[identity] = socket // Create an association between the User identity and this socket.
+  setEventListeners(socket) // We need to setup the server to listen to the various socket events now that the client is authenticated.
+  socket.emit('authenticated') // Let the client know they have been authenticated by emitting an 'authenticated' event.
 
   // Now that the client is online we need to notify all the subscribers.
-  notifySubscribers(identity);
+  notifySubscribers(identity)
 }
 
 function notifySubscribers (identity) {
@@ -58,16 +58,16 @@ function notifySubscribers (identity) {
     if (error) {
       // This is a scilent error that really doesnt impact any of the clients.
       // Its just too bad no one knows your online....
-      debug.log(error.message);
+      debug.log(error.message)
     }
     else {
-      var socket = connectedSockets[subscriber];
+      var socket = connectedSockets[subscriber]
       if (socket && subscriber !== identity) { // If there is a socket with the identity we issue an alert to it.
-        debug.log('Server notification for client (' + socket.id + ') identity (' + socket.__identity__ + ') - ' + identity);
-        socket.emit('online', {'identity': socket.__identity__});
+        debug.log('Server notification for client (' + socket.id + ') identity (' + socket.__identity__ + ') - ' + identity)
+        socket.emit('online', {'identity': socket.__identity__})
       }
     }
-  });
+  })
 }
 
 function authenticateClient (identity, secret, socket) {
@@ -79,33 +79,33 @@ function authenticateClient (identity, secret, socket) {
     // This is not the more robust solution but it will work once the client isn't abusive.
     if (error) {
       debug.log('Socket (' + socket.id + ') provided the server with invalid authentication values.\n' + error.message)
-      socket.emit('wtf'); 
+      socket.emit('wtf') 
     }
     else {
-      debug.log('Successfully authenticated socket (' + socket.id + ') with the server.');
-      connectSocket(identity, socket);
+      debug.log('Successfully authenticated socket (' + socket.id + ') with the server.')
+      connectSocket(identity, socket)
     }
-  });
+  })
 }
 
 function createClient (data, socket) {
-  debug.log('Server is now setting up a new account for client ( '+ socket.id +' )');
+  debug.log('Server is now setting up a new account for client ( '+ socket.id +' )')
   datastore.makeOrUpdateUser(data, function (error) {
     if (error) {
       debug.log('Could not create account for socket (' + socket.id + ').\n' + error.message)
-      socket.emit('wtf');
+      socket.emit('wtf')
     }
     else {
-      debug.log('Successfully created and authenticated socket (' + socket.id + ') with the server.');
-      connectSocket(data.identity, socket);
+      debug.log('Successfully created and authenticated socket (' + socket.id + ') with the server.')
+      connectSocket(data.identity, socket)
     }
-  });
+  })
 }
 
 
 function disconnect (socket) {
   // Remove this socket from the list of connected sockets.
-  delete connectedSockets[socket.__identity__];
+  delete connectedSockets[socket.__identity__]
 }
 
 
@@ -149,25 +149,25 @@ function singleSubscribe (identity, socket) {
 // Sends message directly to a participant based on the identity.
 function sendMessage (identity, message, socket) {
   // Get the participants socket.
-  var participant = connectedSockets[identity];
+  var participant = connectedSockets[identity]
 
   // If this participant has a socket we pipe the message over the socket to them.
   if (participant) {
     datastore.addStatistics({ transmission: 'outgoing', length: message.length, 'event': 'message' })
-    participant.emit('message', { 'message' : message });
-    socket.emit('notification', {type: 'message', delivery: 'successful'});
+    participant.emit('message', { 'message' : message })
+    socket.emit('notification', {type: 'message', delivery: 'successful'})
   } else {
-    socket.emit('notification', {type: 'message', delivery: 'failed'});
+    socket.emit('notification', {type: 'message', delivery: 'failed'})
   }
-};
+}
 
 var setEventListeners = function (socket) {
-  debug.log('Server is now setting up the client for communications');
+  debug.log('Server is now setting up the client for communications')
 
   socket.on('disconnect', function() {
-    debug.log('Event \'disconnect\' fired by client identity ' + socket.__identity__);
-    disconnect(this);
-  });
+    debug.log('Event \'disconnect\' fired by client identity ' + socket.__identity__)
+    disconnect(this)
+  })
 
   // The subscribe event allows clients to subscribe to a list of users
   // if they exsist in the datastore. subscribe will emit the subscribed event
@@ -183,7 +183,7 @@ var setEventListeners = function (socket) {
   })
 
   socket.on('getStats', function () {
-    var socket = this;
+    var socket = this
     datastore.getUsageStatistics(function (error, data) { 
       socket.emit('stats', {'error': error, 'data': data})
     })
@@ -196,7 +196,7 @@ var setEventListeners = function (socket) {
   socket.on('message', function (data) {
     datastore.addStatistics({ transmission: 'incoming', length: JSON.stringify(data).length, 'event': 'message' })
     sendMessage (data.identity, data.message, socket)
-  });
+  })
 }
 
 
@@ -215,12 +215,12 @@ var auth = 'kjlhajkdlhfjhasdnfasdkjflnasdf'
 var config = 'basic'
 var port = 3010
 
-var _ = require('lodash');
-var crypto = require('crypto');
+var _ = require('lodash')
+var crypto = require('crypto')
 var datastore = require('./datastore.js')(auth)
 
 // Holds an up-to-date list of all the connected users.
-var connected = [];
+var connected = []
 
 var debug = {
 	log: function (message) {
@@ -228,28 +228,28 @@ var debug = {
 	}
 }
 
-// Auto generate a random string of lenght len;
+// Auto generate a random string of lenght len
 var autoGen = function(len) {
 		var map = "1234567890poiuytrewqasdfghjklmnbvcxz~!@#$%^&*POIUYTREWQASDFGHJKLMNBVCXZ",
-				str = "";
+				str = ""
 
-		for(var i=0; i<len; ++i) {
-			rand = Math.floor(Math.random() * map.length);
-			str += map[rand];
+		for(var i=0 i<len ++i) {
+			rand = Math.floor(Math.random() * map.length)
+			str += map[rand]
 		}
 
-		return str;
-};
+		return str
+}
 
 var hash = function(password) {
 		var salt = '3EKJ2_@&Ea',
-				saltedpassword = password + salt;
+				saltedpassword = password + salt
 
 		var sha1 = crypto.createHash('sha1').update(saltedpassword),
-				hash = sha1.digest("hex");
+				hash = sha1.digest("hex")
 
-		return hash;
-};
+		return hash
+}
 
 //  Helper function!
 // ==================
@@ -258,70 +258,70 @@ var containsO = function(arr, target) {
     var flag = false
     _.each(arr, function(object) {
       if(_.contains(object, target))
-        flag = true;
-    });
-    return flag;
-};
+        flag = true
+    })
+    return flag
+}
 
 var disconnect = function(socket) {
     // Broadcast removal of socket from list.
     socket.broadcast.emit('alert', { 
       type : FLAGS.REM,
       cid : socket.id,
-    });
+    })
 
     // Update the connection list.
     _.remove(connected, function(client) {
-      return client.cid === socket.id;
-    });
-};
+      return client.cid === socket.id
+    })
+}
 
 var openChat = function(socket, data) {
 
     if (containsO(connected, data.id)) {
       // Generate a room name of 30 chars long.
-      var room_name = autoGen(30);
+      var room_name = autoGen(30)
 
       socket.broadcast.emit('alert', {
         type : FLAGS.CREQ,
         cid : data.id,
         room : room_name,
-      });
+      })
 
-      socket.join(room_name);
+      socket.join(room_name)
 
       socket.emit('alert',{
         type : FLAGS.JOINT,
         room : room_name,
-      });
+      })
     } else {
-      socket.emit('err');
+      socket.emit('err')
     }
-};
+}
 
 var joinRoom = function(socket, data) {
     if(hash(data.nonce) === getNonce(socket.id)) {
-      socket.join(data.room);
+      socket.join(data.room)
       socket.emit('alert',{
         type : FLAGS.JOINT,
         room : data.room,
-      });
-    };
-};
+      })
+    }
+}
 
 var setupClient = function (socket) {
-		debug.log('Server is now setting up the client for communications');
+		debug.log('Server is now setting up the client for communications')
 
 		socket.on('disconnect', function() {
 			debug.log('Event \'disconnect\' fired by client ' + socket.id)
-			//disconnect(this);
-		});
+			//disconnect(this)
+		})
 
 		// The subscribe event allows clients to subscribe to a list of users
 		// if they exsist in the datastore. subscribe will emit the subscribed event
 		// for each successful subscription made on the server.
 		socket.on('subscribe', function (data) {
-			var socket = this; // lets keep a refrence to the socket just in case we need it.
+			var socket = this // lets keep a refrence to the socket just in case we need it.
 
 			if (typeof data !== 'string' && data.length) { // If not string and has length property then assume array was passed.
 				debug.log('Client is trying to subscribe to (' + data.length + ') other users.')
@@ -360,25 +360,25 @@ var setupClient = function (socket) {
 		})
 
 		socket.on('chat', function(data) {
-			openChat(this, data);
-		});
+			openChat(this, data)
+		})
 
 		socket.on('acknowledge', function(data) {
-			joinRoom(this, data);
-		});
+			joinRoom(this, data)
+		})
 
 		socket.on('message', function(data) {
-			debug.log(data);
-			debug.log(this.__data__);
-			socket.broadcast.to(data.room).emit('message', data);
-		});
+			debug.log(data)
+			debug.log(this.__data__)
+			socket.broadcast.to(data.room).emit('message', data)
+		})
 
 		socket.emit('authenticated')
 }
 
 var getNonce = function(id) {
-	return _.find(connected, {cid : id}).nonce;
-};
+	return _.find(connected, {cid : id}).nonce
+}
 
 var secureConnection = function (nonce, socket, fn) {
 	debug.log('Checking validating client identity to server.')
@@ -391,19 +391,19 @@ var secureConnection = function (nonce, socket, fn) {
 }
 
 var connect = function(socket) {
-		// Using autoGen create a nonce of length 20;
-		var nonce = autoGen(20);
+		// Using autoGen create a nonce of length 20
+		var nonce = autoGen(20)
 
 		// Send the client details about themselves as kept by the server.
 		socket.emit('authenticate', {
 			me : socket.id,
 			nonce : nonce,
-		});
+		})
 
 		// If this is not a free package then we must wait for the client to authenticate
 		// before we allow the access to the chat facilities.
 		if (config !== 'free') {
-			debug.log('Non-free package detected. Waiting for authentication to setting up the client for communcations.');
+			debug.log('Non-free package detected. Waiting for authentication to setting up the client for communcations.')
 			
 			// listen to see of the client sends any authentication details.
 			socket.on('identify', function (data){
@@ -414,7 +414,7 @@ var connect = function(socket) {
 				secureConnection(data.nonce, currSocket, function (err){
 					if (err) {
 						debug.log('The client did not pass the nounce test. Ignoring request')
-						return false;
+						return false
 					}
 					else {
 						debug.log('A valid client is trying to authenticate with the server.')
@@ -448,18 +448,18 @@ var connect = function(socket) {
 		else {
 			// Otherwise run the setup procedure and attach all the events
 			// needed for chat and notifications.
-			debug.log('Free package detected. Bypassing authentication.');
+			debug.log('Free package detected. Bypassing authentication.')
 			setupClient(socket)
 		}
 
 		// Add the client to list of connected clients.
 		// Hash the nonce so that other clients can't view it.
-		connected.push({cid : socket.id, nonce : hash(nonce), __socket: socket});
+		connected.push({cid : socket.id, nonce : hash(nonce), __socket: socket})
 
 
 
 		console.log(connected)
-};
+}
 
 
 io.sockets.on('connection', function(socket, data) {
@@ -467,7 +467,7 @@ io.sockets.on('connection', function(socket, data) {
 	// Depending on the configuration of the Server, the client may not be added to
 	// the standard event listeners, muting the client from chat until they authenticate.
 	connect(socket)
-});
+})
 
 
 http.listen(port, function(){
